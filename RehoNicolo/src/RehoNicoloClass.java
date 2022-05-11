897d7df632dd7b172d922a4a7f45be41a86a84f2
Reho Nicolo <gggggg@gggggg.com>
2022-05-11 19:51:40 +0200

    Versione 0:errata

---
+++ RehoNicolo/src/RehoNicoloClass.java
@@ -0,0 +1,32 @@
+/**
+ *	Classe RehoNicoloClass 	 
+ *   @author <b>Reho Nicolo</b>
+ *   <U>4BI</U>
+ *   <i>11\05\2022</i>
+ *   @version 1.0
+ *
+ */
+public class RehoNicoloClass {
+	/**
+	 * Costruttore classe RehoNicoloClass
+	 */
+	
+	public RehoNicoloClass() {}
+	/**
+	 * Sommatoria
+	 * metodo che restituisce la somma dei numeri interi compresi fra l'indice iniziale G 
+	 * @param f indice iniziale
+	 * @return r somma
+	 */
+	public int RehoMet(int f) {
+		int r = 1;
+		for(int i=f; i<0;i++) {
+			r = r*i;
+		}
+		return r;
+	}
+	public static void main(String[]args) {
+		RehoNicoloClass c = new RehoNicoloClass();
+		System.out.println(c.RehoMet(5));
+	}
+}
