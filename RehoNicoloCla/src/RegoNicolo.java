d7d2b3361e25a6adb945687faa9e4979858a3eaa
Reho Nicolo <rehor@DESKTOP-8LAJ5MN.station>
2022-05-12 23:08:20 +0200

    versione 0 : errata

---
+++ RehoNicoloCla/src/RegoNicolo.java
@@ -0,0 +1,32 @@
+/**
+ * classe RegoNicolo
+ * @author <b>Reho Nicolo</b>
+ * <U> 4 BI </U>
+ * <i>12/05/2022</i>
+ * @version 0.0
+ *
+ */
+public class RegoNicolo {
+/**
+ * costruttore RegoNicolo	
+ */
+	public RegoNicolo()
+	{}
+/**
+ * metodo sommatoria	
+ * @param f indice iniziale
+ * @return r sommaotria
+ */
+	public int rehoMet(int f) {
+		int r = 1;
+		for(int i=f; i<0; i++) {
+			r = r*1;
+		}
+		return r;
+	}
+	public static void main(String[] args) {
+		RegoNicolo n = new RegoNicolo();
+		System.out.println(n.rehoMet(5));
+	}
+
+}
